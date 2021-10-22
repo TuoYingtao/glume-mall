7a6ab9e92378a71256cc1291bc7a8246087e4568
TuoYingtao <tuoyingtao@qq.com>
2021-10-22 14:23:17 +0800

    删除废弃包

--- glume-admin/src/main/java/com/glume/glumemall/admin/security/JwtAuthenticationFilter.java
+++ glume-admin/src/main/java/com/glume/glumemall/admin/security/JwtAuthenticationFilter.java
@@ -9,11 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
-import javax.annotation.Resource;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;

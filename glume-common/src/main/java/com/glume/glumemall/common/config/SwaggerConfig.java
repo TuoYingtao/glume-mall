b77e3c395bc32835e8970f482465bbb077d39149
TuoYingtao <tuoyingtao@qq.com>
2021-10-23 16:36:47 +0800

    优化swagger配置

--- glume-common/src/main/java/com/glume/glumemall/common/config/SwaggerConfig.java
+++ glume-common/src/main/java/com/glume/glumemall/common/config/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package com.glume.glumemall.common.config;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.models.auth.In;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,9 +10,9 @@ import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.schema.ModelRef;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Parameter;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -25,6 +27,7 @@ import java.util.List;
 @Configuration
 public class SwaggerConfig {
 
+    /** 是否开启swagger */
     @Value("${swagger.enabled}")
     boolean swaggerEnable;
 
@@ -35,24 +38,61 @@ public class SwaggerConfig {
     public Docket createRestApi() {
 
         return new Docket(DocumentationType.SWAGGER_2)
-                .pathMapping("/")
-                .apiInfo(webApiInfo())
+                // 是否启用Swagger
                 .enable(swaggerEnable)
+                // 用来创建该API的基本信息，展示在文档的页面中（自定义展示的信息）
+                .apiInfo(webApiInfo())
+                // 设置请求的统一前缀
+                .pathMapping("/")
+                // 设置哪些接口暴露给Swagger展示
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.glume.glumemall.admin.controller"))
+                // 扫描所有有注解的api，用这种方式更灵活
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                // 扫描指定包中的swagger注解
+                // .apis(RequestHandlerSelectors.basePackage("com.glume.glumemall.admin.controller"))
+                // 扫描所有 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
+                /* 设置安全模式，swagger可以设置访问token */
+                .securitySchemes(securitySchemes())
+                .securityContexts(securityContexts())
                 .globalOperationParameters(getParameterList());
     }
 
-    public ApiInfo webApiInfo() {
-        return new ApiInfoBuilder().title("glume-mall API 文档")
-                .description("glume-mall 接口文档")
-                .termsOfServiceUrl("https://github.com/TuoYingtao/glume-mall")
-                .version("1.0.0")
-                .license("The Apache License")
-                .licenseUrl("https://github.com/TuoYingtao/glume-mall/blob/main/LICENSE")
-                .build();
+    /**
+     * 安全模式，这里指定token通过Authorization头请求头传递
+     */
+    private List<SecurityScheme> securitySchemes()
+    {
+        List<SecurityScheme> apiKeyList = new ArrayList<SecurityScheme>();
+        apiKeyList.add(new ApiKey(headerToken, headerToken, In.HEADER.toValue()));
+        return apiKeyList;
+    }
+
+    /**
+     * 安全上下文
+     */
+    private List<SecurityContext> securityContexts()
+    {
+        List<SecurityContext> securityContexts = new ArrayList<>();
+        securityContexts.add(
+                SecurityContext.builder()
+                        .securityReferences(defaultAuth())
+                        .build());
+        return securityContexts;
+    }
+
+    /**
+     * 默认的安全上引用
+     */
+    private List<SecurityReference> defaultAuth()
+    {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        List<SecurityReference> securityReferences = new ArrayList<>();
+        securityReferences.add(new SecurityReference("Authorization", authorizationScopes));
+        return securityReferences;
     }
 
     /**
@@ -66,5 +106,25 @@ public class SwaggerConfig {
                 .parameterType("header").required(false).build();
         arrayList.add(parameterBuilder.build());
         return arrayList;
+    }
+
+    /**
+     * 添加摘要信息
+     * @return
+     */
+    public ApiInfo webApiInfo() {
+        return new ApiInfoBuilder()
+                // 设置标题
+                .title("标题：glume-mall API 文档")
+                // 描述
+                .description("描述：glume-mall 接口文档")
+                .termsOfServiceUrl("https://github.com/TuoYingtao/glume-mall")
+                // 版本
+                .version("版本号：1.0.0")
+                // 许可证
+                .license("许可证：The Apache License")
+                // 许可证地址
+                .licenseUrl("https://github.com/TuoYingtao/glume-mall/blob/main/LICENSE")
+                .build();
     }
 }

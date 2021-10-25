ffb76963228c052365308a7dd05bdc4a35f78912
TuoYingtao <tuoyingtao@qq.com>
2021-10-25 14:23:32 +0800

    定义Redis常量

--- glume-common/src/main/java/com/glume/glumemall/common/constant/RedisConstant.java
+++ glume-common/src/main/java/com/glume/glumemall/common/constant/RedisConstant.java
@@ -5,6 +5,8 @@ package com.glume.glumemall.common.constant;
  * @create 2021-10-22 13:38
  */
 public interface RedisConstant {
+    /* 用户信息 */
+    String USER_INFO_AND_MENU = "USER_INFO_AND_MENU";
     /* 用户权限 */
     String REDIS_AUTHORITY_KEY = "REDIS_AUTHORITY_KEY";
     /* code验证码 */

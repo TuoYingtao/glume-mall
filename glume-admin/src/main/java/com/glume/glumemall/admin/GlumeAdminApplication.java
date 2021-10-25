153849c10121e7ce9cffff6725fddbfdc2204db7
TuoYingtao <tuoyingtao@qq.com>
2021-10-25 14:23:06 +0800

    扫描公共包下的工具类

--- glume-admin/src/main/java/com/glume/glumemall/admin/GlumeAdminApplication.java
+++ glume-admin/src/main/java/com/glume/glumemall/admin/GlumeAdminApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan({"com.glume.glumemall.admin","com.glume.glumemall.common.config"})
+@ComponentScan({"com.glume.glumemall.admin","com.glume.glumemall.common.config","com.glume.glumemall.common.utils"})
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GlumeAdminApplication {

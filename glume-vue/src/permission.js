4f8b62aafb23820ae0631da44070d2886083eb56
TuoYingtao <tuoyingtao@qq.com>
2021-10-25 18:24:55 +0800

    前端：路由守卫判断处理

--- glume-vue/src/permission.js
+++ glume-vue/src/permission.js
@@ -1,4 +1,5 @@
 import router from './router'
+import store from './store'
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
 import { getToken } from '@/utils/auth'
@@ -7,17 +8,54 @@ NProgress.configure({ showSpinner: false })
 
 router.beforeEach((to, from, next) => {
     NProgress.start()
-    if (getToken()) {
-      next()
+    // if (getToken()) {
+    //   next()
+    // } else {
+    //   // 没有token
+    //   if (to.path == '/login') {
+    //     next();
+    //   } else {
+    //     next(`/login`) // 否则全部重定向到登录页
+    //     NProgress.done()
+    //   }
+    // }
+
+  if (getToken()) {
+    // to.meta.title && store.dispatch('settings/setTitle', to.meta.title)
+    if (to.path === '/login') {
+      next({ path: '/' })
+      NProgress.done()
     } else {
-      // 没有token
-      if (to.path == '/login') {
-        next();
+      if (store.getters.roles.length === 0) {
+        // 判断当前用户是否已拉取完user_info信息
+        store.dispatch('GetInfo').then(res => {
+          store.dispatch('GenerateRoutes',res.data.menus).then(accessRoutes => {
+            console.log(accessRoutes)
+            // 根据roles权限生成可访问的路由表
+            router.addRoutes(accessRoutes) // 动态添加可访问路由表
+            next({ ...to, replace: true }) // hack方法 确保addRoutes已完成
+          })
+        }).catch(err => {
+          store.dispatch('LogOut').then(() => {
+            Message.error(err)
+            next({ path: '/' })
+          })
+        })
       } else {
-        next(`/login`) // 否则全部重定向到登录页
-        NProgress.done()
+        next()
       }
     }
+  } else {
+    // 没有token
+    console.log(to.path)
+    // 在免登录白名单，直接进入
+    if (to.path == '/login') {
+      next()
+    } else {
+      next(`/login?redirect=${to.fullPath}`) // 否则全部重定向到登录页
+      NProgress.done()
+    }
+  }
 })
 
 router.afterEach(() => {

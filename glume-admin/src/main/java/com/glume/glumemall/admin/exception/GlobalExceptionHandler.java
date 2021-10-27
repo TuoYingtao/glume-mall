ca3f2a28500405e563f60e67b7a839d1447f63ad
BeautifulGirl230 <542986649@qq.com>
2021-10-27 22:54:07 +0800

    定义Validation全局异常处理

--- glume-admin/src/main/java/com/glume/glumemall/admin/exception/GlobalExceptionHandler.java
+++ glume-admin/src/main/java/com/glume/glumemall/admin/exception/GlobalExceptionHandler.java
@@ -7,12 +7,21 @@ import io.jsonwebtoken.JwtException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
 /**
  * 全局异常处理器
@@ -50,7 +59,7 @@ public class GlobalExceptionHandler {
     public R handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException e, HttpServletRequest request) {
         String requestURI = request.getRequestURI();
         LOGGER.error("请求地址'{}',不支持'{}'请求", requestURI, e.getMethod());
-        return R.error(e.getMessage());
+        return R.error("不支持" + e.getMessage() + "请求");
     }
 
     /**
@@ -58,20 +67,53 @@ public class GlobalExceptionHandler {
      */
     @ExceptionHandler(ServiceException.class)
     public R handleServiceException(ServiceException e, HttpServletRequest request) {
-        LOGGER.error(e.getMessage(), e);
+        String requestURI = request.getRequestURI();
+        LOGGER.error("请求地址'{}',业务异常'{}'",requestURI, e.getMessage(), e);
         Integer code = e.getCode();
         return code == null ? R.error(code, e.getMessage()) : R.error(e.getMessage());
     }
 
     /**
-     * 自定义验证异常
+     * 校验异常：前段以json格式有效
      */
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Object handleMethodArgumentNotValidException(MethodArgumentNotValidException e)
-    {
-        LOGGER.error(e.getMessage(), e);
+    public Object handleMethodArgumentNotValidException(MethodArgumentNotValidException e,HttpServletRequest request) {
+        String requestURI = request.getRequestURI();
+        LOGGER.error("请求地址'{}',校验异常'{}'",requestURI, e.getMessage(), e);
         String message = e.getBindingResult().getFieldError().getDefaultMessage();
         return R.error(message);
+    }
+
+    /**
+     * 校验异常：表单提交有效，对于以json格式提交将会失效
+     */
+    @ExceptionHandler(BindException.class)
+    public R handleBindException(BindException e,HttpServletRequest request) {
+        String requestURI = request.getRequestURI();
+        LOGGER.error("请求地址'{}',校验异常'{}'",requestURI,e.getMessage(), e);
+        String message = "";
+        BindingResult bindingResult = e.getBindingResult();
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            message += fieldError.getDefaultMessage() + "!";
+        }
+        return R.error(message);
+    }
+
+    /**
+     * 校验异常：参数上加@RequestParam或参数加@NotBlank @NotNull等
+     */
+    @ExceptionHandler(ConstraintViolationException.class)
+    public R ConstraintViolationExceptionHandler(ConstraintViolationException e,HttpServletRequest request) {
+        String requestURI = request.getRequestURI();
+        Set<ConstraintViolation<?>> constraintViolations = e.getConstraintViolations();
+        Iterator<ConstraintViolation<?>> iterator = constraintViolations.iterator();
+        List<String> msgList = new ArrayList<>();
+        while (iterator.hasNext()) {
+            ConstraintViolation<?> cvl = iterator.next();
+            msgList.add(cvl.getMessageTemplate());
+        }
+        LOGGER.error("请求地址'{}',校验异常'{}'", requestURI, e.getMessage(), e);
+        return R.error(String.join(",",msgList));
     }
 
     /**

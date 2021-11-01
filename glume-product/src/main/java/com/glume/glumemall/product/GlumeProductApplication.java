813110ccdb28377b23d0da56b8cb7c9250d07547
TuoYingtao <tuoyingtao@163.com>
2021-11-01 14:09:22 +0800

    商品服务开启nacos注入、引入common

--- glume-product/src/main/java/com/glume/glumemall/product/GlumeProductApplication.java
+++ glume-product/src/main/java/com/glume/glumemall/product/GlumeProductApplication.java
@@ -2,8 +2,14 @@ package com.glume.glumemall.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
+@EnableDiscoveryClient
 @SpringBootApplication
+@ComponentScan({"com.glume.glumemall.product",
+        "com.glume.glumemall.common.config",
+        "com.glume.glumemall.common.utils"})
 public class GlumeProductApplication {
 
     public static void main(String[] args) {

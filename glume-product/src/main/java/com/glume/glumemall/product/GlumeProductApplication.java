0a95462ad29c56795a183e5538d74d29643413f2
TuoYingtao <tuoyingtao@163.com>
2021-11-04 14:35:53 +0800

    解决MyBatis-Plus分页总条数显示为：0

--- glume-product/src/main/java/com/glume/glumemall/product/GlumeProductApplication.java
+++ glume-product/src/main/java/com/glume/glumemall/product/GlumeProductApplication.java
@@ -3,9 +3,14 @@ package com.glume.glumemall.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@ComponentScans({
+        @ComponentScan("com.glume.common.mybatis.config")
+})
 public class GlumeProductApplication {
 
     public static void main(String[] args) {

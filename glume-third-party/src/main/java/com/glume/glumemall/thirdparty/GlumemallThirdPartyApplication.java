3dfcb47b750d5f54673210febe8435198308c480
BeautifulGirl230 <542986649@qq.com>
2021-11-04 23:27:46 +0800

    完成第三方服务oss服务器签名接口在web端直传

--- glume-third-party/src/main/java/com/glume/glumemall/thirdparty/GlumemallThirdPartyApplication.java
+++ glume-third-party/src/main/java/com/glume/glumemall/thirdparty/GlumemallThirdPartyApplication.java
@@ -2,8 +2,15 @@ package com.glume.glumemall.thirdparty;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 
+@EnableDiscoveryClient
 @SpringBootApplication
+@ComponentScans({
+        @ComponentScan("com.glume.common.core")
+})
 public class GlumemallThirdPartyApplication {
 
     public static void main(String[] args) {

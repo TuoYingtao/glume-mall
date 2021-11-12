c3fa2c37b7ddd80037163fc5dbcb09591571a1c0
TuoYingtao <tuoyingtao@163.com>
2021-11-12 16:35:34 +0800

    增长OSS签名过期时间

--- glume-third-party/src/main/java/com/glume/glumemall/thirdparty/controller/OssController.java
+++ glume-third-party/src/main/java/com/glume/glumemall/thirdparty/controller/OssController.java
@@ -47,7 +47,7 @@ public class OssController {
 
         Map<String, String> respMap = null;
         try {
-            long expireTime = 30;
+            long expireTime = 600;
             long expireEndTime = System.currentTimeMillis() + expireTime * 1000;
             Date expiration = new Date(expireEndTime);
             // PostObject请求最大可支持的文件大小为5 GB，即CONTENT_LENGTH_RANGE为5*1024*1024*1024。

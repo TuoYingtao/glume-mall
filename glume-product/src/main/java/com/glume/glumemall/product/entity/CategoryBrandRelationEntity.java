9ca5a9cbd6ecfba1c76a2dd6b07410defa3933d4
TuoYingtao <tuoyingtao@163.com>
2021-11-15 13:57:15 +0800

    品牌&分类关联实体类JSR303参数校验

--- glume-product/src/main/java/com/glume/glumemall/product/entity/CategoryBrandRelationEntity.java
+++ glume-product/src/main/java/com/glume/glumemall/product/entity/CategoryBrandRelationEntity.java
@@ -2,8 +2,11 @@ package com.glume.glumemall.product.entity;
 
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.glume.common.core.annotation.valid.IDGroup;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 /**
@@ -22,22 +25,26 @@ public class CategoryBrandRelationEntity implements Serializable {
 	 * id
 	 */
 	@TableId
+	@NotNull(message = "品牌&分类关联ID不能为空",groups = IDGroup.class)
 	private Long id;
 	/**
 	 * 品牌ID
 	 */
+	@NotNull(message = "品牌ID不能为空")
 	private Long brandId;
 	/**
-	 * 分能ID
+	 * 分类ID
 	 */
+	@NotNull(message = "分类ID不能为空")
 	private Long catelogId;
 	/**
 	 * 品牌名称
 	 */
+	@NotBlank(message = "品牌名称不能为空")
 	private String brandName;
 	/**
 	 * 分类名称
 	 */
+	@NotBlank(message = "分类名称不能为空")
 	private String catelogName;
-
 }

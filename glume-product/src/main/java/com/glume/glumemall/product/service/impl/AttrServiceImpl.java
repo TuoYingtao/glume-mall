e82e5fdef4b386c8db68b15294c336cf5c56d879
TuoYingtao <tuoyingtao@163.com>
2021-11-16 11:24:25 +0800

    优化商品属性查询列表信息返回分组和分类名称

--- glume-product/src/main/java/com/glume/glumemall/product/service/impl/AttrServiceImpl.java
+++ glume-product/src/main/java/com/glume/glumemall/product/service/impl/AttrServiceImpl.java
@@ -3,12 +3,22 @@ package com.glume.glumemall.product.service.impl;
 import com.glume.common.core.utils.StringUtils;
 import com.glume.common.mybatis.PageUtils;
 import com.glume.common.mybatis.Query;
+import com.glume.glumemall.product.dao.AttrAttrgroupRelationDao;
+import com.glume.glumemall.product.dao.AttrGroupDao;
+import com.glume.glumemall.product.dao.CategoryDao;
+import com.glume.glumemall.product.entity.AttrAttrgroupRelationEntity;
+import com.glume.glumemall.product.entity.AttrGroupEntity;
+import com.glume.glumemall.product.entity.CategoryEntity;
 import com.glume.glumemall.product.service.AttrAttrgroupRelationService;
+import com.glume.glumemall.product.vo.AttrRespVo;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -25,13 +35,20 @@ public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements
     @Autowired
     AttrAttrgroupRelationService attrAttrgroupRelationService;
 
+    @Autowired
+    AttrAttrgroupRelationDao relationDao;
+
+    @Autowired
+    AttrGroupDao attrGroupDao;
+
+    @Autowired
+    CategoryDao categoryDao;
+
     @Override
     public PageUtils queryPage(Map<String, Object> params, Long catelogId) {
+        IPage<AttrEntity> page = null;
         if (catelogId == 0) {
-            IPage<AttrEntity> page = this.page(
-                    new Query<AttrEntity>().getPage(params),
-                    new QueryWrapper<AttrEntity>());
-            return new PageUtils(page);
+            page = this.page(new Query<AttrEntity>().getPage(params), new QueryWrapper<AttrEntity>());
         } else {
             QueryWrapper<AttrEntity> attrEntityQueryWrapper = new QueryWrapper<AttrEntity>().eq("catelog_id", catelogId);
             if (StringUtils.isNotNull(params.get("key"))) {
@@ -40,9 +57,28 @@ public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements
                     obj.like("attr_name",key).or().like("value_select",key).or().like("icon",key);
                 });
             }
-            IPage<AttrEntity> page = this.page(new Query<AttrEntity>().getPage(params),attrEntityQueryWrapper);
-            return new PageUtils(page);
+            page = this.page(new Query<AttrEntity>().getPage(params),attrEntityQueryWrapper);
         }
+        PageUtils pageUtils = new PageUtils(page);
+        List<AttrEntity> records = page.getRecords();
+        List<AttrRespVo> list = records.stream().map(attrEntity -> {
+            AttrRespVo attrRespVo = new AttrRespVo();
+            BeanUtils.copyProperties(attrEntity, attrRespVo);
+            /** 设置分类分组信息 */
+            AttrAttrgroupRelationEntity attrAttrgroupRelationEntity =
+                    relationDao.selectOne(new QueryWrapper<AttrAttrgroupRelationEntity>().eq("attr_id", attrRespVo.getAttrId()));
+            if (StringUtils.isNotNull(attrAttrgroupRelationEntity)) {
+                AttrGroupEntity attrGroupEntity = attrGroupDao.selectById(attrAttrgroupRelationEntity.getAttrGroupId());
+                attrRespVo.setAttrGroupName(attrGroupEntity.getAttrGroupName());
+            }
+            CategoryEntity categoryEntity = categoryDao.selectById(attrEntity.getCatelogId());
+            if (StringUtils.isNotNull(categoryEntity)) {
+                attrRespVo.setCatelogName(categoryEntity.getName());
+            }
+            return attrRespVo;
+        }).collect(Collectors.toList());
+        pageUtils.setList(list);
+        return pageUtils;
     }
 
     @Override

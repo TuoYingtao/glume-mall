ec881427c7b10a53e736235c4da43076d13613f0
BeautifulGirl230 <542986649@qq.com>
2021-11-18 21:59:47 +0800

    添加会员服务相关配置

--- glume-member/src/main/java/com/glume/glumemall/member/GlumeMemberApplication.java
+++ glume-member/src/main/java/com/glume/glumemall/member/GlumeMemberApplication.java
@@ -4,11 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 
 
 @EnableFeignClients(basePackages = "com.glume.glumemall.member.feign")
 @EnableDiscoveryClient
 @SpringBootApplication
+@ComponentScans({
+        @ComponentScan("com.glume.common.core"),
+        @ComponentScan("com.glume.common.mybatis.config"),
+        @ComponentScan("com.glume.common.swagger.config"),
+        @ComponentScan("com.glume.common.validator.config")
+})
 public class GlumeMemberApplication {
 
     public static void main(String[] args) {

88cab8346d976b1b469aadbed52f072c2c728621
BeautifulGirl230 <542986649@qq.com>
2021-11-18 23:21:59 +0800

    会员等级controller完成基本的增删改查接口

--- glume-member/src/main/java/com/glume/glumemall/member/controller/MemberLevelController.java
+++ glume-member/src/main/java/com/glume/glumemall/member/controller/MemberLevelController.java
@@ -3,12 +3,12 @@ package com.glume.glumemall.member.controller;
 import java.util.Arrays;
 import java.util.Map;
 
+import com.glume.common.core.annotation.valid.AddGroup;
+import com.glume.common.core.annotation.valid.IDGroup;
+import com.glume.common.core.annotation.valid.UpdateGroup;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import com.glume.glumemall.member.entity.MemberLevelEntity;
 import com.glume.glumemall.member.service.MemberLevelService;
@@ -33,52 +33,52 @@ public class MemberLevelController {
     /**
      * 列表
      */
-    @RequestMapping("/list")
+    @GetMapping("/list")
     public R list(@RequestParam Map<String, Object> params){
         PageUtils page = memberLevelService.queryPage(params);
 
-        return R.ok().put("page", page);
+        return R.ok().put("data", page);
     }
 
 
     /**
      * 信息
      */
-    @RequestMapping("/info/{id}")
+    @GetMapping("/info/{id}")
     public R info(@PathVariable("id") Long id){
 		MemberLevelEntity memberLevel = memberLevelService.getById(id);
 
-        return R.ok().put("memberLevel", memberLevel);
+        return R.ok().put("data", memberLevel);
     }
 
     /**
      * 保存
      */
-    @RequestMapping("/save")
-    public R save(@RequestBody MemberLevelEntity memberLevel){
+    @PostMapping("/save")
+    public R save(@Validated(AddGroup.class) MemberLevelEntity memberLevel){
 		memberLevelService.save(memberLevel);
 
-        return R.ok();
+        return R.ok("会员等级保存成功！");
     }
 
     /**
      * 修改
      */
-    @RequestMapping("/update")
-    public R update(@RequestBody MemberLevelEntity memberLevel){
+    @PutMapping("/update")
+    public R update(@Validated(UpdateGroup.class) MemberLevelEntity memberLevel){
 		memberLevelService.updateById(memberLevel);
 
-        return R.ok();
+        return R.ok("会员等级修改成功！");
     }
 
     /**
      * 删除
      */
-    @RequestMapping("/delete")
-    public R delete(@RequestBody Long[] ids){
+    @DeleteMapping("/delete")
+    public R delete(@Validated(IDGroup.class) Long[] ids){
 		memberLevelService.removeByIds(Arrays.asList(ids));
 
-        return R.ok();
+        return R.ok("会员等级删除成功！");
     }
 
 }

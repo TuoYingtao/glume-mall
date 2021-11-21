412d364e10727d52489ea796003faf41964d3ac3
BeautifulGirl230 <542986649@qq.com>
2021-11-21 22:20:45 +0800

    构建SPU与SKU基本实体信息、VO实现、增删改查

---
+++ glume-product/src/main/java/com/glume/glumemall/product/controller/SpuInfoController.java
@@ -0,0 +1,82 @@
+package com.glume.glumemall.product.controller;
+
+import com.glume.common.core.utils.R;
+import com.glume.common.mybatis.PageUtils;
+import com.glume.glumemall.product.entity.SpuInfoEntity;
+import com.glume.glumemall.product.service.SpuInfoService;
+import com.glume.glumemall.product.vo.SpuSaveVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.Map;
+
+
+
+/**
+ * spu信息
+ *
+ * @author tuoyingtao
+ * @email tuoyingtao@163.com
+ * @date 2021-10-13 14:25:13
+ */
+@RestController
+@RequestMapping("product/spuinfo")
+public class SpuInfoController {
+    @Autowired
+    private SpuInfoService spuInfoService;
+
+    /**
+     * 列表
+     */
+    @RequestMapping("/list")
+    public R list(@RequestParam Map<String, Object> params){
+        PageUtils page = spuInfoService.queryPage(params);
+
+        return R.ok().put("page", page);
+    }
+
+
+    /**
+     * 信息
+     */
+    @RequestMapping("/info/{id}")
+    public R info(@PathVariable("id") Long id){
+		SpuInfoEntity spuInfo = spuInfoService.getById(id);
+
+        return R.ok().put("spuInfo", spuInfo);
+    }
+
+    /**
+     * 保存
+     */
+    @RequestMapping("/save")
+    public R save(@RequestBody SpuSaveVo spuSaveVo){
+		//spuInfoService.save(spuInfo);
+
+        spuInfoService.saveSpuInfo(spuSaveVo);
+
+        return R.ok();
+    }
+
+    /**
+     * 修改
+     */
+    @RequestMapping("/update")
+    public R update(@RequestBody SpuInfoEntity spuInfo){
+		spuInfoService.updateById(spuInfo);
+
+        return R.ok();
+    }
+
+    /**
+     * 删除
+     */
+    @RequestMapping("/delete")
+    public R delete(@RequestBody Long[] ids){
+		spuInfoService.removeByIds(Arrays.asList(ids));
+
+        return R.ok();
+    }
+
+}

3152d06b887678e3e0b69af294b92949f33cbd2d
BeautifulGirl230 <542986649@qq.com>
2021-11-28 19:17:13 +0800

    ware库存服务开启事务、远程调用、注册中心、核心包注解

--- glume-ware/src/main/java/com/glume/glumemall/ware/GlumeWareApplication.java
+++ glume-ware/src/main/java/com/glume/glumemall/ware/GlumeWareApplication.java
@@ -3,9 +3,23 @@ package com.glume.glumemall.ware;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableDiscoveryClient
 @MapperScan("com.glume.glumemall.ware.dao")
 @SpringBootApplication
+@EnableTransactionManagement
+@EnableFeignClients
+@ComponentScans({
+        @ComponentScan("com.glume.common.core"),
+        @ComponentScan("com.glume.common.mybatis.config"),
+        @ComponentScan("com.glume.common.swagger.config"),
+        @ComponentScan("com.glume.common.validator.config")
+})
 public class GlumeWareApplication {
 
     public static void main(String[] args) {

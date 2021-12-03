fdea608aeaaf38bb8dff2ed1cfd08d3869545544
BeautifulGirl230 <542986649@qq.com>
2021-12-03 23:07:36 +0800

    product服务添加Vo实体

---
+++ glume-product/src/main/java/com/glume/glumemall/product/vo/item/AttrValueWithSkuIdVo.java
@@ -0,0 +1,19 @@
+package com.glume.glumemall.product.vo.item;
+
+import lombok.Data;
+
+/**
+ * @author TuoYingtao
+ * @create 2021-12-03 21:53
+ */
+@Data
+public class AttrValueWithSkuIdVo {
+    /**
+     * 属性值
+     */
+    private String attrValue;
+    /**
+     * sku_id
+     */
+    private String skuIds;
+}

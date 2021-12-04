50e088ff2b925a8fa4c12e34554999ff015c2a6d
TuoYingtao <tuoyingtao@163.com>
2021-12-04 11:26:53 +0800

    product服务创建ThreadPoolExecutor线程池并注入进容器中

---
+++ glume-product/src/main/java/com/glume/glumemall/product/config/ThreadPoolConfigProperties.java
@@ -0,0 +1,34 @@
+package com.glume.glumemall.product.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * 线程池配置实体
+ * @author tuoyingtao
+ * @create 2021-12-04 9:52
+ */
+@ConfigurationProperties(prefix = "glumemall.thread")
+@Component
+@Data
+public class ThreadPoolConfigProperties {
+    /**
+     * 核心线程数
+     */
+    private Integer corePoolSize;
+    /**
+     * 最大线程数
+     */
+    private Integer maximumPoolSize;
+    /**
+     * 活动时间
+     */
+    private Integer keepAliveTime;
+
+    public ThreadPoolConfigProperties() {
+        this.corePoolSize = 50;
+        this.maximumPoolSize = 200;
+        this.keepAliveTime = 10;
+    }
+}

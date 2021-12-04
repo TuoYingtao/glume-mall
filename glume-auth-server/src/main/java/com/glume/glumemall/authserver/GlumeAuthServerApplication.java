6e592ab65648d625c5658bcf42a60596da9dbb2c
TuoYingtao <tuoyingtao@163.com>
2021-12-04 11:52:13 +0800

    glumemall 添加认证中心glume-auth-server

---
+++ glume-auth-server/src/main/java/com/glume/glumemall/authserver/GlumeAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.glume.glumemall.authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlumeAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlumeAuthServerApplication.class, args);
+	}
+
+}
---
+++ glume-auth-server/src/test/java/com/glume/glumemall/authserver/GlumeAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glume.glumemall.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlumeAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

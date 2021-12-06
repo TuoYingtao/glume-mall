9d66402d338d4e7437b6ca8becab35ced230b806
TuoYingtao <tuoyingtao@163.com>
2021-12-06 15:32:10 +0800

    auth-server导入Redis依赖、公共核心工具类扫描、配置thymeleaf、配置Redis连接地址、配置Nacos注册中心地址

--- glume-auth-server/src/main/java/com/glume/glumemall/authserver/GlumeAuthServerApplication.java
+++ glume-auth-server/src/main/java/com/glume/glumemall/authserver/GlumeAuthServerApplication.java
@@ -4,10 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
+@ComponentScans({
+		@ComponentScan("com.glume.common.core"),
+})
 public class GlumeAuthServerApplication {
 
 	public static void main(String[] args) {

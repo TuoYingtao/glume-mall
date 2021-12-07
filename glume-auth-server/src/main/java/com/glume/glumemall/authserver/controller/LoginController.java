7a9d1e01e4e8d3fb11626dadc77383ca1a3b4f5f
TuoYingtao <tuoyingtao@163.com>
2021-12-07 16:01:43 +0800

    glume-auth-server远程调用glume-member服务的注册功能

--- glume-auth-server/src/main/java/com/glume/glumemall/authserver/controller/LoginController.java
+++ glume-auth-server/src/main/java/com/glume/glumemall/authserver/controller/LoginController.java
@@ -4,6 +4,7 @@ import com.glume.common.core.constant.AuthServerConstant;
 import com.glume.common.core.constant.HttpStatus;
 import com.glume.common.core.utils.R;
 import com.glume.common.core.utils.StringUtils;
+import com.glume.glumemall.authserver.feign.MemberFeignService;
 import com.glume.glumemall.authserver.feign.ThirdPartFeignService;
 import com.glume.glumemall.authserver.vo.UserRegisterVo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
@@ -36,18 +38,24 @@ public class LoginController {
     @Autowired
     RedisTemplate redisTemplate;
 
+    @Autowired
+    MemberFeignService memberFeignService;
+
     @ResponseBody
     @GetMapping("/sms/sendcode")
     public R sendCode(@RequestParam("mobile") String mobile) {
-        String redisMobileCode = redisTemplate.opsForValue().get(AuthServerConstant.SMS_CODE_CACHE_PREFIX + mobile).toString();
-        if (!StringUtils.isNotNull(redisMobileCode)) {
+        if (StringUtils.isNotNull(redisTemplate.opsForValue().get(AuthServerConstant.SMS_CODE_CACHE_PREFIX + mobile))) {
+            String redisMobileCode = redisTemplate.opsForValue().get(AuthServerConstant.SMS_CODE_CACHE_PREFIX + mobile).toString();
             long redisMobileCodeTime = Long.parseLong(redisMobileCode.split("_")[1]);
             if (System.currentTimeMillis() - redisMobileCodeTime < (60 * 1000)) {
                 return R.error(HttpStatus.BizCodeEnum.SMS_CODE_EXCEPTION.getCode(), HttpStatus.BizCodeEnum.SMS_CODE_EXCEPTION.getMsg());
             }
         }
-        String code = UUID.randomUUID().toString().substring(0, 5) + "_" + System.currentTimeMillis();
-        redisTemplate.opsForValue().set(AuthServerConstant.SMS_CODE_CACHE_PREFIX + mobile,code,5, TimeUnit.MINUTES);
+        String code = UUID.randomUUID().toString().replaceAll("[a-zA-Z]","").substring(0, 5);
+        System.out.println(code);
+        String redisCode = code + "_" + System.currentTimeMillis();
+        System.out.println(redisCode.getClass());
+        redisTemplate.opsForValue().set(AuthServerConstant.SMS_CODE_CACHE_PREFIX + mobile,redisCode,60, TimeUnit.MINUTES);
         thirdPartFeignService.sendCode(mobile,code);
         return R.ok();
     }
@@ -66,6 +74,30 @@ public class LoginController {
             redirectAttributes.addFlashAttribute("errors",errors);
             return "redirect:http://auth.glumemall.com/register.html";
         }
-        return "redirect:/login.html";
+        String code = userRegisterVo.getCode();
+        if (StringUtils.isNotNull(redisTemplate.opsForValue().get(AuthServerConstant.SMS_CODE_CACHE_PREFIX + userRegisterVo.getMobile()))) {
+            String s = redisTemplate.opsForValue().get(AuthServerConstant.SMS_CODE_CACHE_PREFIX + userRegisterVo.getMobile()).toString();
+            if (code.equals(s.split("_")[0])) {
+                redisTemplate.delete(AuthServerConstant.SMS_CODE_CACHE_PREFIX + userRegisterVo.getMobile());
+                R register = memberFeignService.register(userRegisterVo);
+                if (register.getCode() == 200) {
+                    return "redirect:http://auth.glumemall.com/login.html";
+                } else {
+                    Map<String, Object> errors = new HashMap<>();
+                    errors.put("msg",register.get("msg").toString());
+                    redirectAttributes.addFlashAttribute("errors",errors);
+                    return "redirect:http://auth.glumemall.com/register.html";
+                }
+            } else {
+                Map<String, Object> errors = new HashMap<>();
+                redirectAttributes.addFlashAttribute("errors",errors);
+                return "redirect:http://auth.glumemall.com/register.html";
+            }
+        } else {
+            Map<String, Object> errors = new HashMap<>();
+            errors.put("msg","验证码不正确！");
+            redirectAttributes.addFlashAttribute("errors",errors);
+            return "redirect:http://auth.glumemall.com/register.html";
+        }
     }
 }
---
+++ glume-auth-server/src/main/java/com/glume/glumemall/authserver/feign/MemberFeignService.java
@@ -0,0 +1,18 @@
+package com.glume.glumemall.authserver.feign;
+
+import com.glume.common.core.utils.R;
+import com.glume.glumemall.authserver.vo.UserRegisterVo;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+/**
+ * @author tuoyingtao
+ * @create 2021-12-07 11:38
+ */
+@FeignClient("glume-member")
+public interface MemberFeignService {
+
+    @PostMapping("/member/member/register")
+    public R register(@RequestBody UserRegisterVo userRegisterVo);
+}

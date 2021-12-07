242905c6e597ec9051643a25ec7f998d14415bff
TuoYingtao <tuoyingtao@163.com>
2021-12-07 16:00:54 +0800

    glume-auth-server测试Redis缓存sms

--- glume-auth-server/src/test/java/com/glume/glumemall/authserver/GlumeAuthServerApplicationTests.java
+++ glume-auth-server/src/test/java/com/glume/glumemall/authserver/GlumeAuthServerApplicationTests.java
@@ -1,13 +1,21 @@
 package com.glume.glumemall.authserver;
 
+import com.glume.common.core.constant.AuthServerConstant;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootTest
 class GlumeAuthServerApplicationTests {
 
+	@Autowired
+	RedisTemplate redisTemplate;
+
 	@Test
 	void contextLoads() {
+		String s = redisTemplate.opsForValue().get(AuthServerConstant.SMS_CODE_CACHE_PREFIX + "15207447965").toString();
+		System.out.println(s);
 	}
 
 }

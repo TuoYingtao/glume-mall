5a130f4e19ed16d6402df263b565c5812059c216
TuoYingtao <tuoyingtao@163.com>
2021-12-09 17:00:37 +0800

    glume-order服务Rabbit配置开启消息确认机制

--- glume-order/src/main/java/com/glume/glumemall/order/config/RabbitConfig.java
+++ glume-order/src/main/java/com/glume/glumemall/order/config/RabbitConfig.java
@@ -1,19 +1,69 @@
 package com.glume.glumemall.order.config;
 
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
+
 /**
+ * Rabbit 配置
  * @author tuoyingtao
  * @create 2021-12-09 10:34
  */
 @Configuration
 public class RabbitConfig {
 
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+    /**
+     * 使用JOSN序列化机制，进行消息转换
+     * @return
+     */
     @Bean
     public MessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
+    }
+
+    /**
+     * 定制RabbitTemplate
+     */
+    @PostConstruct //RabbitConfig 对象创建完成以后执行这个方法
+    public void initRabbitTemplate() {
+        rabbitTemplate.setConfirmCallback(new RabbitTemplate.ConfirmCallback() {
+            /**
+             * 只要消息抵达Broker就 ack == true
+             * @param correlationData 当前消息唯一关联数据（消息的唯一ID）
+             * @param ack 消息是否成功收到
+             * @param cause 失败原因
+             */
+            @Override
+            public void confirm(CorrelationData correlationData, boolean ack, String cause) {
+                System.out.println("confirm...correlationData[" + correlationData + "]==>ack[" + ack + "]==>cause[ " + cause + "]");
+            }
+        });
+        /**
+         * 设置消息抵达队列的确认回调
+         */
+        rabbitTemplate.setReturnCallback(new RabbitTemplate.ReturnCallback() {
+            /**
+             * 只要消息没用投递给知道的队列，就触发这个失败回调
+             * @param message 投递失败的消息详细信息
+             * @param replyCode 回复的状态码
+             * @param replyText 回复的文本内容
+             * @param exchange 当时消息是发给那个交换机
+             * @param routingKey 当时消息是发给那个路由键
+             */
+            @Override
+            public void returnedMessage(Message message, int replyCode, String replyText, String exchange, String routingKey) {
+                System.out.println("Fail...Message[" + message + "]==>replyCode[" + replyCode + "]==>replyText[ " + replyText + "]==>exchange[" + exchange + "]==>routingKey[" + routingKey + "]");
+            }
+        });
     }
 }

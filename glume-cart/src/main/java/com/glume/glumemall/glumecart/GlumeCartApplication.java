49efb0b6aa34c86332efb8ddf1c1eef9994c8e41
TuoYingtao <tuoyingtao@163.com>
2021-12-10 17:53:54 +0800

    glume-cart启动类开启naocs、OpenFeign相关注解

---
+++ glume-cart/src/main/java/com/glume/glumemall/glumecart/GlumeCartApplication.java
@@ -0,0 +1,18 @@
+package com.glume.glumemall.glumecart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class GlumeCartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlumeCartApplication.class, args);
+    }
+
+}
---
+++ glume-cart/src/test/java/com/glume/glumemall/glumecart/GlumeCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glume.glumemall.glumecart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlumeCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

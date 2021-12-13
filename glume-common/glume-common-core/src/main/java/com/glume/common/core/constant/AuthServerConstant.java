8c15d0cd4b3c44cee91842eed09c023a248cb776
TuoYingtao <tuoyingtao@163.com>
2021-12-13 17:56:16 +0800

    glume-auth-server定义登录session保存到redis的常量

--- glume-common/glume-common-core/src/main/java/com/glume/common/core/constant/AuthServerConstant.java
+++ glume-common/glume-common-core/src/main/java/com/glume/common/core/constant/AuthServerConstant.java
@@ -7,4 +7,5 @@ package com.glume.common.core.constant;
  */
 public class AuthServerConstant {
     public static final String SMS_CODE_CACHE_PREFIX = "sms:code:";
+    public static final String LOGIN_USER = "loginUser";
 }

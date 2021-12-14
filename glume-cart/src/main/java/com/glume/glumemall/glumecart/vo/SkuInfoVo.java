74f7733b3162aff6546e17f2f6149ec74baa310a
TuoYingtao <tuoyingtao@163.com>
2021-12-14 18:02:47 +0800

    glume-cart 封装SKU信息实体VO

---
+++ glume-cart/src/main/java/com/glume/glumemall/glumecart/vo/SkuInfoVo.java
@@ -0,0 +1,58 @@
+package com.glume.glumemall.glumecart.vo;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+/**
+ * glume-product SKU信息
+ * @author tuoyingtao
+ * @create 2021-12-14 14:20
+ */
+@Data
+public class SkuInfoVo {
+    /**
+     * skuId
+     */
+    private Long skuId;
+    /**
+     * spuId
+     */
+    private Long spuId;
+    /**
+     * sku名称
+     */
+    private String skuName;
+    /**
+     * sku介绍描述
+     */
+    private String skuDesc;
+    /**
+     * 所属分类id
+     */
+    private Long catalogId;
+    /**
+     * 品牌id
+     */
+    private Long brandId;
+    /**
+     * 默认图片
+     */
+    private String skuDefaultImg;
+    /**
+     * 标题
+     */
+    private String skuTitle;
+    /**
+     * 副标题
+     */
+    private String skuSubtitle;
+    /**
+     * 价格
+     */
+    private BigDecimal price;
+    /**
+     * 销量
+     */
+    private Long saleCount;
+}

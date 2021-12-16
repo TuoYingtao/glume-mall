fa9079a4612d9a3ae012f6f6ecc8796e558c047c
TuoYingtao <tuoyingtao@163.com>
2021-12-16 10:25:20 +0800

    glume-order 线程池配置

---
+++ glume-order/src/main/java/com/glume/glumemall/order/config/MyThreadConfig.java
@@ -0,0 +1,29 @@
+package com.glume.glumemall.order.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 线程池配置
+ * @author tuoyingtao
+ * @create 2021-12-04 9:45
+ */
+//@EnableConfigurationProperties(ThreadPoolConfigProperties.class)
+@Configuration
+public class MyThreadConfig {
+
+    @Bean
+    public ThreadPoolExecutor threadPoolExecutor(ThreadPoolConfigProperties pool) {
+        return new ThreadPoolExecutor(pool.getCorePoolSize(),
+                pool.getMaximumPoolSize(),
+                pool.getKeepAliveTime(),
+                TimeUnit.SECONDS,
+                new LinkedBlockingDeque<>(100000),
+                Executors.defaultThreadFactory(),new ThreadPoolExecutor.AbortPolicy());
+    }
+}

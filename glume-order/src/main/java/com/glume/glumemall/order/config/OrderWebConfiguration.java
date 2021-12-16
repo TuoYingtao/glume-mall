fdad7221a7685d7dc56b04345e543fec41229223
TuoYingtao <tuoyingtao@163.com>
2021-12-16 10:30:19 +0800

    glume-order 订单拦截器判断当前用户是否登录，否则重定向登录页

---
+++ glume-order/src/main/java/com/glume/glumemall/order/config/OrderWebConfiguration.java
@@ -0,0 +1,23 @@
+package com.glume.glumemall.order.config;
+
+import com.glume.glumemall.order.interceptor.LoginUserInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author tuoyingtao
+ * @create 2021-12-16 9:54
+ */
+@Configuration
+public class OrderWebConfiguration implements WebMvcConfigurer {
+
+    @Autowired
+    LoginUserInterceptor loginUserInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginUserInterceptor).addPathPatterns("/**");
+    }
+}
---
+++ glume-order/src/main/java/com/glume/glumemall/order/interceptor/LoginUserInterceptor.java
@@ -0,0 +1,42 @@
+package com.glume.glumemall.order.interceptor;
+
+import com.glume.common.core.constant.AuthServerConstant;
+import com.glume.common.core.to.MemberRespTo;
+import com.glume.common.core.utils.StringUtils;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 订单拦截器
+ * @author tuoyingtao
+ * @create 2021-12-16 9:52
+ */
+@Component
+public class LoginUserInterceptor implements HandlerInterceptor {
+
+    public static ThreadLocal<MemberRespTo> toThreadLocal = new ThreadLocal<>();
+
+    /**
+     * 前置拦截 判断用户是否登录
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        MemberRespTo attribute = (MemberRespTo) request.getSession().getAttribute(AuthServerConstant.LOGIN_USER);
+        if (StringUtils.isNotNull(attribute)) {
+            toThreadLocal.set(attribute);
+            return true;
+        } else {
+            request.getSession().setAttribute("msg","请先进行登录");
+            response.sendRedirect("http://auth.glumemall.com/login.html");
+            return false;
+        }
+    }
+}

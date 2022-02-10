c1687d1dad7c06a1d5d4cf01913021341dc60f61
TuoYingtao <tuoyingtao@163.com>
2022-02-10 17:50:48 +0800

    优化Redis缓存字符串乱码问题

---
+++ glume-product/src/main/java/com/glume/glumemall/product/config/RedisConfiguration.java
@@ -0,0 +1,30 @@
+package com.glume.glumemall.product.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @author tuoyingtao
+ * @create 2022-02-10 14:45
+ */
+@Configuration
+public class RedisConfiguration {
+
+    @Autowired
+    private RedisConnectionFactory redisConnectionFactory;
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        return redisTemplate;
+    }
+}

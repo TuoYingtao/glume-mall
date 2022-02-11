30513067e97fe3e5d5fd1b2362498102c6404ad0
TuoYingtao <tuoyingtao@163.com>
2022-02-11 17:23:26 +0800

    优化订单确认实体Vo

--- glume-order/src/main/java/com/glume/glumemall/order/vo/OrderConfirmVo.java
+++ glume-order/src/main/java/com/glume/glumemall/order/vo/OrderConfirmVo.java
@@ -1,7 +1,8 @@
 package com.glume.glumemall.order.vo;
 
 import com.glume.common.core.utils.StringUtils;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -11,24 +12,27 @@ import java.util.List;
  * @author tuoyingtao
  * @create 2021-12-16 10:31
  */
-@Data
 public class OrderConfirmVo {
 
     /**
      * 会员地址 ums_member_receive_address
      */
+    @Setter @Getter
     private List<MemberAddressVo> address;
     /**
      * 所选的购物项 pms_sku_info pms_sku_sale_attr_value
      */
+    @Setter @Getter
     private List<OrderItemVo> items;
     /**
-     * 积分 ums_member
+     * 优惠卷、积分 ums_member
      */
+    @Setter @Getter
     private Integer integration;
     /**
      * 防重令牌
      */
+    @Setter @Getter
     private String orderToken;
     /**
      * 订单总额

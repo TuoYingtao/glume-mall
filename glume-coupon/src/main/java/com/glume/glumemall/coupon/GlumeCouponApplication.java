bc1ca48f822f01804757ae3a3e224170e78a7af7
TuoYingtao <tuoyingtao@163.com>
2022-02-18 18:21:27 +0800

    积分服务启用分布式事务

--- glume-coupon/src/main/java/com/glume/glumemall/coupon/GlumeCouponApplication.java
+++ glume-coupon/src/main/java/com/glume/glumemall/coupon/GlumeCouponApplication.java
@@ -3,9 +3,14 @@ package com.glume.glumemall.coupon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@ComponentScans({
+        @ComponentScan({"com.glume.common.seata"})
+})
 public class GlumeCouponApplication {
 
     public static void main(String[] args) {

9c3f4c6914cb96f08619e0e05ac761a36c14bbc5
TuoYingtao <tuoyingtao@163.com>
2022-02-18 18:10:46 +0800

    添加公用的分布式事务模块

---
+++ glume-common/glume-common-seata/src/main/java/com/glume/common/seata/config/MySeataConfig.java
@@ -0,0 +1,32 @@
+package com.glume.common.seata.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import io.seata.rm.datasource.DataSourceProxy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+
+import javax.sql.DataSource;
+
+/**
+ * Seata 分布式事务配置
+ * @author tuoyingtao
+ * @create 2022-02-16 15:58
+ */
+@Configuration
+public class MySeataConfig {
+
+    @Autowired
+    DataSourceProperties dataSourceProperties;
+
+    @Bean
+    public DataSource dataSource(DataSourceProperties dataSourceProperties) {
+        HikariDataSource dataSource = dataSourceProperties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
+        if (StringUtils.hasText(dataSourceProperties.getName())) {
+            dataSource.setPoolName(dataSourceProperties.getName());
+        }
+        return new DataSourceProxy(dataSource);
+    }
+}

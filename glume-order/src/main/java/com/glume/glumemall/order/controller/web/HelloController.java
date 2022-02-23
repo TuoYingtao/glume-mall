c3abfb22b078ff48ef1d19eeb073b00b1a6e0f11
TuoYingtao <tuoyingtao@163.com>
2022-02-23 17:40:50 +0800

    测试消息延迟队列：模拟订单提交

--- glume-order/src/main/java/com/glume/glumemall/order/controller/web/HelloController.java
+++ glume-order/src/main/java/com/glume/glumemall/order/controller/web/HelloController.java
@@ -1,8 +1,15 @@
 package com.glume.glumemall.order.controller.web;
 
+import com.glume.glumemall.order.entity.OrderEntity;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
+import java.util.UUID;
 
 /**
  * @author tuoyingtao
@@ -11,8 +18,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class HelloController {
 
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
     @GetMapping("/{page}.html")
     public String listPage(@PathVariable("page") String page) {
         return page;
+    }
+
+    @ResponseBody
+    @GetMapping("/test/createOrder")
+    public String createOrderTest() {
+        OrderEntity orderEntity = new OrderEntity();
+        orderEntity.setOrderSn(UUID.randomUUID().toString());
+        orderEntity.setModifyTime(new Date());
+        // 创建订单成功，给rabbitMQ发送消息
+        rabbitTemplate.convertAndSend("order-event-exchange","order.create.order",orderEntity);
+        return "ok";
     }
 }

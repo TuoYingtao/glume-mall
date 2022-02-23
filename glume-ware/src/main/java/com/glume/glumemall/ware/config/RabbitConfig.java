6edc26d7f083afd134c682e9d76ab2a6099d35a7
TuoYingtao <tuoyingtao@163.com>
2022-02-23 18:35:45 +0800

    创建 rabbitMQ 配置

---
+++ glume-ware/src/main/java/com/glume/glumemall/ware/config/RabbitConfig.java
@@ -0,0 +1,71 @@
+package com.glume.glumemall.ware.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * rabbitMQ 配置
+ * @author tuoyingtao
+ * @create 2022-02-23 17:10
+ */
+@Configuration
+public class RabbitConfig {
+
+    /**
+     * 使用JSON序列化机制，进行消息转换
+     */
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public Exchange stockEventExchange() {
+        return new TopicExchange("stock-event-exchange",true,false);
+    }
+
+    @Bean
+    public Queue stockReleaseStockQueue() {
+        return new Queue("stock.release.stock.queue",true,false,false);
+    }
+
+    @Bean
+    public Queue stockDelayQueue() {
+        Map<String, Object> arguments = new HashMap<>();
+        // 设置死信路由
+        arguments.put("x-dead-letter-exchange","stock-event-exchange");
+        // 设置路由键
+        arguments.put("x-dead-letter-routing-key","stock.locked");
+        // 过期时间
+        arguments.put("x-message-ttl",120000);
+        return new Queue("stock.delay.queue",true,false,false, arguments);
+    }
+
+    @Bean
+    public Binding stockReleaseBinding() {
+        return new Binding("stock.release.stock.queue",
+                Binding.DestinationType.QUEUE,
+                "stock-event-exchange",
+                "stock.release.#",
+                null);
+    }
+
+    @Bean
+    public Binding stockLockedBinding() {
+        return new Binding("stock.delay.queue",
+                Binding.DestinationType.QUEUE,
+                "stock-event-exchange",
+                "stock.locked",
+                null);
+    }
+
+}

93c88af0086329dcd123d3382ddc9c2d17ba6a01
TuoYingtao <tuoyingtao@163.com>
2022-02-23 18:35:30 +0800

    创建 RabbitMQ 库存工作单To

---
+++ glume-common/glume-common-core/src/main/java/com/glume/common/core/to/mq/StockLockedTo.java
@@ -0,0 +1,23 @@
+package com.glume.common.core.to.mq;
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * 库存工作单To
+ * @author tuoyingtao
+ * @create 2022-02-23 18:23
+ */
+@Data
+public class StockLockedTo {
+
+    /**
+     * 库存工作单ID
+     */
+    private Long id;
+    /**
+     * 库存工作单详情ID
+     */
+    private Long detailId;
+}

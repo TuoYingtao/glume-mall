24e585f15dea7a3bba60f79d183c3448c1e047c4
TuoYingtao <tuoyingtao@163.com>
2022-02-25 16:36:40 +0800

    RabbitMQ 配置文件合并

--- glume-order/src/main/java/com/glume/glumemall/order/config/RabbitConfig.java
+++ glume-order/src/main/java/com/glume/glumemall/order/config/RabbitConfig.java
@@ -2,7 +2,7 @@ package com.glume.glumemall.order.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -12,9 +12,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Rabbit 配置
+ * 容器中的 Binding, Queue, Exchange 都会自动创建（RabbitMQ 没有的情况）
  * @author tuoyingtao
  * @create 2021-12-09 10:34
  */
@@ -25,18 +28,63 @@ public class RabbitConfig {
     @Autowired
     RabbitTemplate rabbitTemplate;
 
-    /**
-     * 使用JOSN序列化机制，进行消息转换
-     * @return
-     */
+    /** 使用JOSN序列化机制，进行消息转换 */
     @Bean
     public MessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
     }
 
-    /**
-     * 定制RabbitTemplate
-     */
+    @Bean
+    public Exchange orderEventExchange() {
+        return new TopicExchange("order-event-exchange", true, false);
+    }
+
+    @Bean
+    public Queue orderReleaseOrderQueue() {
+        return new Queue("order.release.order.queue", true, false, false);
+    }
+
+    @Bean
+    public Queue orderDelayQueue() {
+        Map<String,Object> arguments = new HashMap<>();
+        // 设置死信路由
+        arguments.put("x-dead-letter-exchange","order-event-exchange");
+        // 设置路由键
+        arguments.put("x-dead-letter-routing-key","order.release.order");
+        // 过期时间
+        arguments.put("x-message-ttl",60000);
+        return new Queue("order.delay.queue", true, false, false,arguments);
+    }
+
+    @Bean
+    public Binding orderCreateOrderBinding() {
+        Binding binding = new Binding("order.delay.queue",
+                Binding.DestinationType.QUEUE,
+                "order-event-exchange",
+                "order.create.order",null);
+        return binding;
+    }
+
+    @Bean
+    public Binding orderReleaseOrderBinding() {
+        Binding binding = new Binding("order.release.order.queue",
+                Binding.DestinationType.QUEUE,
+                "order-event-exchange",
+                "order.release.order",null);
+        return binding;
+    }
+
+    /** 绑定订单释放直接通知库存进行释放 */
+    @Bean
+    public Binding orderReleaseOtherBinding() {
+        Binding binding = new Binding("stock.release.stock.queue",
+                Binding.DestinationType.QUEUE,
+                "order-event-exchange",
+                "order.release.other.#",null);
+        return binding;
+    }
+
+    /** 定制RabbitTemplate */
     @PostConstruct //@PostConstruct注解：当RabbitConfig 对象创建完成以后执行这个方法
     public void initRabbitTemplate() {
         rabbitTemplate.setConfirmCallback(new RabbitTemplate.ConfirmCallback() {
@@ -51,9 +99,8 @@ public class RabbitConfig {
                 LOGGER.info("confirm...correlationData[" + correlationData + "]==>ack[" + ack + "]==>cause[ " + cause + "]");
             }
         });
-        /**
-         * 设置消息抵达队列的确认回调
-         */
+
+        /** 设置消息抵达队列的确认回调 */
         rabbitTemplate.setReturnCallback(new RabbitTemplate.ReturnCallback() {
             /**
              * 只要消息没用投递给知道的队列，就触发这个失败回调

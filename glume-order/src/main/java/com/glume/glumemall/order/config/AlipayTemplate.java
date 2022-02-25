c8e09f824976f32daba19963480f8f9dd7e590e0
TuoYingtao <tuoyingtao@163.com>
2022-02-25 18:28:27 +0800

    订单支付响应订单列表：配置支付成功后，跳转到会员页

--- glume-order/src/main/java/com/glume/glumemall/order/config/AlipayTemplate.java
+++ glume-order/src/main/java/com/glume/glumemall/order/config/AlipayTemplate.java
@@ -34,7 +34,7 @@ public class AlipayTemplate {
 
     // 页面跳转同步通知页面路径 需http://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
     //同步通知，支付成功，一般跳转到成功页
-    private String return_url;
+    private String return_url = "http://member.glumemall.com/memberOrder.html";
 
     // 签名方式
     private String sign_type = "RSA2";

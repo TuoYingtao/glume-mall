6003c433553514605fcc3dbf0aa833920457083b
TuoYingtao <tuoyingtao@163.com>
2022-03-01 17:47:49 +0800

    每日秒杀-定时任务：定时任务配置类

---
+++ glume-seckill/src/main/java/com/glume/glumemall/seckill/config/ScheduledConfig.java
@@ -0,0 +1,16 @@
+package com.glume.glumemall.seckill.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * 定时任务配置类
+ * @author tuoyingtao
+ * @create 2022-03-01 16:59
+ */
+@Configuration
+@EnableAsync
+@EnableScheduling
+public class ScheduledConfig {
+}

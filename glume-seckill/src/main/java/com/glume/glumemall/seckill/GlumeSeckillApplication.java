680300306a33325ff641ab0434e213769a60b845
TuoYingtao <tuoyingtao@163.com>
2022-03-03 18:26:29 +0800

    每日秒杀-并发下单处理：整合RabbitMQ、session登录认证

--- glume-seckill/src/main/java/com/glume/glumemall/seckill/GlumeSeckillApplication.java
+++ glume-seckill/src/main/java/com/glume/glumemall/seckill/GlumeSeckillApplication.java
@@ -2,12 +2,13 @@ package com.glume.glumemall.seckill;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class GlumeSeckillApplication {
 
     public static void main(String[] args) {

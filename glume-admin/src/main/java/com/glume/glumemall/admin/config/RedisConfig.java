4dd5f6ac091a2b39ed1ee84651d1b9049368cb77
TuoYingtao <tuoyingtao@163.com>
2022-03-08 18:02:58 +0800

    admin后台服务：配置RedisTemplate模板缓存数据格式；redis 常量配置

---
+++ glume-admin/src/main/java/com/glume/glumemall/admin/config/RedisConfig.java
@@ -0,0 +1,31 @@
+package com.glume.glumemall.admin.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * Redis配置
+ * @author tuoyingtao
+ * @create 2022-03-08 13:42
+ */
+@Configuration
+public class RedisConfig {
+
+    @Autowired
+    RedisConnectionFactory redisConnectionFactory;
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        return redisTemplate;
+    }
+}
--- glume-common/glume-common-core/src/main/java/com/glume/common/core/constant/RedisConstant.java
+++ glume-common/glume-common-core/src/main/java/com/glume/common/core/constant/RedisConstant.java
@@ -1,14 +1,15 @@
 package com.glume.common.core.constant;
 
 /**
+ * redis 常量
  * @author tuoyingtao
  * @create 2021-10-22 13:38
  */
 public interface RedisConstant {
     /* 用户信息 */
-    String USER_INFO_AND_MENU = "USER_INFO_AND_MENU";
+    String USER_INFO_AND_MENU = "admin:userinfo:menu:";
     /* 用户权限 */
-    String REDIS_AUTHORITY_KEY = "REDIS_AUTHORITY_KEY";
+    String REDIS_AUTHORITY_KEY = "admin:authorit:key:";
     /* code验证码 */
-    String CAPTCHA_KEY = "captcha";
+    String CAPTCHA_KEY = "admin:captcha:";
 }

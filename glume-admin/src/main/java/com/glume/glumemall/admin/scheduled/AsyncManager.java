5977808692fffde96b853329cb010a3b2e532e9c
TuoYingtao <tuoyingtao@163.com>
2022-03-10 18:09:24 +0800

    添加异步任务管理器

---
+++ glume-admin/src/main/java/com/glume/glumemall/admin/scheduled/AsyncManager.java
@@ -0,0 +1,46 @@
+package com.glume.glumemall.admin.scheduled;
+
+import com.glume.common.core.utils.SpringUtils;
+import com.glume.common.core.utils.Threads;
+
+import java.util.TimerTask;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 异步任务管理器
+ * @author tuoyingtao
+ * @create 2022-03-10 16:57
+ */
+public class AsyncManager {
+
+    public static AsyncManager me = new AsyncManager();
+    /**
+     * 操作延迟10毫秒
+     */
+    private final Integer OPERATE_DELAY_TIME = 10;
+    /**
+     * 异步操作任务调度线程池
+     */
+    private ScheduledExecutorService executor = SpringUtils.getBean(ScheduledExecutorService.class);
+
+    public AsyncManager() {
+    }
+
+    public static AsyncManager me() {
+        return me;
+    }
+    /**
+     * 执行任务
+     * @param task 任务
+     */
+    public void executor(TimerTask timerTask) {
+        executor.schedule(timerTask,OPERATE_DELAY_TIME, TimeUnit.MILLISECONDS);
+    }
+    /**
+     * 停止任务线程池
+     */
+    public void shutdown() {
+        Threads.shutdownAndAwaitTermination(executor);
+    }
+}

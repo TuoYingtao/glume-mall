8cdc87d0da1e040728f3c04c9b2ceae0d65fb517
TuoYingtao <tuoyingtao@163.com>
2022-03-12 18:33:16 +0800

    admin后台服务优惠营销-秒杀活动主题：admin用户实体TO

---
+++ glume-coupon/src/main/java/com/glume/glumemall/coupon/to/AdminUserTo.java
@@ -0,0 +1,46 @@
+package com.glume.glumemall.coupon.to;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.glume.common.core.annotation.Logic;
+import com.glume.common.core.annotation.valid.AddGroup;
+import com.glume.common.core.annotation.valid.IDGroup;
+import com.glume.common.core.annotation.valid.UpdateGroup;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.*;
+import java.util.Date;
+
+/**
+ * admin 用户实体
+ * @author tuoyingtao
+ * @create 2022-03-12 16:53
+ */
+@Data
+public class AdminUserTo {
+
+    private Long userId;
+    /**
+     * 用户名
+     */
+    private String username;
+    /**
+     * 邮箱
+     */
+    private String email;
+    /**
+     * 手机号
+     */
+    private String mobile;
+    /**
+     * 状态  0：禁用   1：正常
+     */
+    private Integer status;
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+}

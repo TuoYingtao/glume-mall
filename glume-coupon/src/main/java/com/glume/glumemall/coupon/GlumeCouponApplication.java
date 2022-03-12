c8cd852c645df556829112e5504d2dc7d0b20c27
TuoYingtao <tuoyingtao@163.com>
2022-03-12 18:27:00 +0800

    admin后台服务优惠营销：活动场次列表筛选、请求参数验证

--- glume-coupon/src/main/java/com/glume/glumemall/coupon/GlumeCouponApplication.java
+++ glume-coupon/src/main/java/com/glume/glumemall/coupon/GlumeCouponApplication.java
@@ -9,7 +9,9 @@ import org.springframework.context.annotation.ComponentScans;
 @EnableDiscoveryClient
 @SpringBootApplication
 @ComponentScans({
-        @ComponentScan({"com.glume.common.seata"})
+        @ComponentScan("com.glume.common.core"),
+        @ComponentScan({"com.glume.common.seata"}),
+        @ComponentScan("com.glume.common.validator.config"),
 })
 public class GlumeCouponApplication {
 

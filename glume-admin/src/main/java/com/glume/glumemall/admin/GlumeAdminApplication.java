fde61b2cd13f8baa0854a3b644e218fe88360193
BeautifulGirl230 <542986649@qq.com>
2022-03-17 12:11:31 +0800

    admin后台服务优惠营销-秒杀活动主题：配置链路追踪、feign远程调用

--- glume-admin/src/main/java/com/glume/glumemall/admin/GlumeAdminApplication.java
+++ glume-admin/src/main/java/com/glume/glumemall/admin/GlumeAdminApplication.java
@@ -3,9 +3,11 @@ package com.glume.glumemall.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 
+@EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
 @ComponentScans({

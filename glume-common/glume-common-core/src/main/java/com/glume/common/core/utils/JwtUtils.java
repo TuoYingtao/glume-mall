1b138ea1f79ec4f7845cedd5cbd71815fbfb0068
BeautifulGirl230 <542986649@qq.com>
2022-03-17 12:01:27 +0800

    admin后台服务优惠营销-秒杀活动主题：删除FeignException异常类

--- glume-common/glume-common-core/src/main/java/com/glume/common/core/utils/JwtUtils.java
+++ glume-common/glume-common-core/src/main/java/com/glume/common/core/utils/JwtUtils.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.Data;
+import lombok.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 

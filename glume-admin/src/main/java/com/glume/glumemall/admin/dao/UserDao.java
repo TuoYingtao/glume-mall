3ee45461808ceb363c6b43396978ef23de2ea7bf
TuoYingtao <tuoyingtao@163.com>
2022-04-13 09:48:14 +0800

    admin服务-管理员列表DAO：重置密码、自定义SQL分页查询

--- glume-admin/src/main/java/com/glume/glumemall/admin/dao/UserDao.java
+++ glume-admin/src/main/java/com/glume/glumemall/admin/dao/UserDao.java
@@ -1,17 +1,24 @@
 package com.glume.glumemall.admin.dao;
 
-import com.glume.glumemall.admin.entity.UserEntity;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.Constants;
+import com.glume.glumemall.admin.entity.UserEntity;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * 系统用户
- * 
+ *
  * @author tuoyingtao
  * @email tuoyingtao@163.com
  * @date 2021-10-18 09:31:33
  */
 @Mapper
 public interface UserDao extends BaseMapper<UserEntity> {
-	
+
+    Integer updateResetPassword(@Param("userId") Long userId, @Param("password") String password);
+
+    IPage<UserEntity> myPageList(IPage<UserEntity> page, @Param(Constants.WRAPPER) QueryWrapper<UserEntity> userEntityQueryWrapper);
 }

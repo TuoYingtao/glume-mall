464dad13c8da8b991c846c78f2c985d525ee335b
TuoYingtao <tuoyingtao@163.com>
2022-04-13 17:32:44 +0800

    admin服务-在线用户列表：用户令牌缓存在 Redis 中

--- glume-common/glume-common-core/src/main/java/com/glume/common/core/constant/RedisConstant.java
+++ glume-common/glume-common-core/src/main/java/com/glume/common/core/constant/RedisConstant.java
@@ -8,10 +8,16 @@ package com.glume.common.core.constant;
 public interface RedisConstant {
     /** 用户信息 */
     String USER_INFO = "admin:userinfo:";
+
     /** 角色菜单 */
     String ROLE_MENU = "admin:role:menu:";
+
     /** 用户权限 */
     String REDIS_AUTHORITY_KEY = "admin:authorit:key:";
+
     /** code验证码 */
     String CAPTCHA_KEY = "admin:captcha:";
+
+    /** 用户黑名单 */
+    String BLACKLIST_KEY = "admin:blacklist:";
 }

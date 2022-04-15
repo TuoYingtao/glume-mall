93f64458f5e152b5bed3f2bfd4ec1361242cd6e1
TuoYingtao <tuoyingtao@163.com>
2022-04-15 09:52:45 +0800

    admin服务-在线用户列表：优化登录成功后记录在线

--- glume-admin/src/main/java/com/glume/glumemall/admin/security/handler/LoginSuccessHandler.java
+++ glume-admin/src/main/java/com/glume/glumemall/admin/security/handler/LoginSuccessHandler.java
@@ -1,5 +1,7 @@
 package com.glume.glumemall.admin.security.handler;
 
+import cn.hutool.http.useragent.UserAgent;
+import cn.hutool.http.useragent.UserAgentUtil;
 import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.glume.common.core.constant.HttpStatus;
@@ -49,25 +51,38 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) {
+        // 创建用户令牌
         String token = createToken(authentication);
+        // 设置在线用户信息
         LoginEntity loginEntity = new LoginEntity();
         loginEntity.setUsername(authentication.getName());
         loginEntity.setToken(token);
         WebAuthenticationDetails webAuthenticationDetails = (WebAuthenticationDetails) authentication.getDetails();
         loginEntity.setIp(webAuthenticationDetails.getRemoteAddress());
         loginEntity.setCreateTime(new Date());
+        UserAgent parse = UserAgentUtil.parse(httpServletRequest.getHeader("User-Agent"));
+        loginEntity.setBrowser(parse.getBrowser().getName());
+        loginEntity.setEngine(parse.getEngine().getName());
+        loginEntity.setEngineVersion(parse.getEngineVersion());
+        loginEntity.setVersion(parse.getVersion());
+        loginEntity.setOs(parse.getOs().getName());
+        loginEntity.setPlatform(parse.getPlatform().toString());
+
         BoundHashOperations hashOps = redisTemplate.boundHashOps(USER_ON_LIEN);
         LoginEntity loginServiceOne = loginService.getOne(new QueryWrapper<LoginEntity>().eq("username", authentication.getName()));
+        // 判断当前登录用户是否已在登录列表
         if (StringUtils.isNotNull(loginServiceOne)) {
-            QueryWrapper<LoginEntity> wrapper = new QueryWrapper<>();
-            wrapper.eq("username",authentication.getName());
-            loginService.update(loginEntity,wrapper);
+            // 存在，更新用户令牌
+            loginService.update(loginEntity,new QueryWrapper<LoginEntity>().eq("username",authentication.getName()));
+            // 删除旧缓存
             if (hashOps.hasKey(authentication.getName())) {
                 hashOps.delete(authentication.getName());
             }
         } else {
+            // 不存在，加入在线列表
             loginService.save(loginEntity);
         }
+        // 将信息存入缓存中
         hashOps.put(authentication.getName(),JSON.toJSONString(loginEntity));
         R result = R.ok()
                 .put("data",new HashMap<String,Object>(){{put("token",token);}})

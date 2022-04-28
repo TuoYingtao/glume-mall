4c443b6963b9cbb5a11f680077ae4af6eeaf1f89
TuoYingtao <tuoyingtao@163.com>
2022-04-28 18:26:20 +0800

    admin服务-Quartz动态定时任务：基本的增删改查、修改Job状态、立即执行一次Job

--- glume-admin/src/main/java/com/glume/glumemall/admin/service/ScheduleJobService.java
+++ glume-admin/src/main/java/com/glume/glumemall/admin/service/ScheduleJobService.java
@@ -3,7 +3,9 @@ package com.glume.glumemall.admin.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.glume.common.mybatis.PageUtils;
 import com.glume.glumemall.admin.entity.ScheduleJobEntity;
+import org.quartz.SchedulerException;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -14,4 +16,16 @@ import java.util.Map;
 public interface ScheduleJobService extends IService<ScheduleJobEntity> {
 
     PageUtils queryPage(Map<String,Object> params);
+
+    Integer saveJob(ScheduleJobEntity scheduleJobEntity) throws SchedulerException;
+
+    Integer updateJobById(ScheduleJobEntity scheduleJobEntity) throws SchedulerException;
+
+    void removeJobByIds(List<Long> jobIds) throws SchedulerException;
+
+    Integer switchStatus(Long jobId) throws SchedulerException;
+
+    void runJob(Long jobId) throws SchedulerException;
+
+    ScheduleJobEntity getInfoById(Long jobId);
 }
